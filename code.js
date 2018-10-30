4f306049ce23a067d6a110f200b33270d3abc9b6
Tess10553 <lt.tess.yu@gmail.com>
2018-10-30 20:52:50 +0800

    ch2.6

---
+++ code.js
@@ -0,0 +1,18 @@
+// var 
+var name = 'John Doe';
+console.log(name);
+// var can be changed
+name = 'Jake Rockson';
+console.log(name);
+
+// init var
+var greeting;
+console.log(greeting);
+greeting = 'Vake';
+console.log(greeting);
+
+//letters,numbers,_, $
+//can not start with number
+
+// multi words var
+var firstName = 'Jack';
