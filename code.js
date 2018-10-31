f216891090fad0dadf303cc82264776920fabfaf
Tess10553 <lt.tess.yu@gmail.com>
2018-10-31 23:39:39 +0800

    ch2.7

--- code.js
+++ code.js
@@ -1,18 +1,35 @@
-// var 
+// // var 
+// var name = 'John Doe';
+// console.log(name);
+// // var can be changed
+// name = 'Jake Rockson';
+// console.log(name);
+
+// // init var
+// var greeting;
+// console.log(greeting);
+// greeting = 'Vake';
+// console.log(greeting);
+
+// //letters,numbers,_, $
+// //can not start with number
+
+// // multi words var
+// var firstName = 'Jack';
+
+// Command+ / = comment and uncomment
+//LET
 var name = 'John Doe';
 console.log(name);
 // var can be changed
 name = 'Jake Rockson';
 console.log(name);
 
-// init var
-var greeting;
-console.log(greeting);
-greeting = 'Vake';
-console.log(greeting);
+// String
+ const name = 'John D';
+ // Number
+ const age = 45;
+ //boolean
+ const = has kid = true;
+ 
 
-//letters,numbers,_, $
-//can not start with number
-
-// multi words var
-var firstName = 'Jack';
